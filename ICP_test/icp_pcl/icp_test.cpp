d3a6199aec9c5ad1142608e1fce70697f70ddb27
haeyeoni <haeyeon8669@gmail.com>
2020-03-31 15:48:43 +0900

    modified

--- ICP_test/icp_pcl/icp_test.cpp
+++ ICP_test/icp_pcl/icp_test.cpp
@@ -12,13 +12,12 @@
 #include <pcl/registration/transformation_estimation_lm.h>
 #include <pcl/filters/uniform_sampling.h>
 
+#include <pcl/registration/default_convergence_criteria.h>
+
 int main ( int argc, char** argv )
 {
   pcl::PCLPointCloud2::Ptr source_cloud (new pcl::PCLPointCloud2 ());
   pcl::PCLPointCloud2::Ptr source_filtered (new pcl::PCLPointCloud2 ());
-  pcl::PCLPointCloud2::Ptr target_cloud (new pcl::PCLPointCloud2 ());
-  pcl::PCLPointCloud2::Ptr target_filtered (new pcl::PCLPointCloud2 ());
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_icp_done (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr source (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr target (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PCDReader reader;
@@ -29,31 +28,36 @@ int main ( int argc, char** argv )
   pcl::ExtractIndices<pcl::PointXYZ> extract;
   pcl::SACSegmentation<pcl::PointXYZ> seg;
   Eigen::Matrix4f transformation_matrix = Eigen::Matrix4f::Identity ();
-  pcl::CorrespondencesPtr correspondences(new pcl::Correspondences);
-  pcl::CorrespondencesPtr rejected_correspondences(new pcl::Correspondences);
-  pcl::registration::TransformationEstimationLM<pcl::PointXYZ, pcl::PointXYZ, double> transformation;
-  pcl::registration::TransformationEstimationLM<pcl::PointXYZ, pcl::PointXYZ, double>::Matrix4 trs_matrix;
-  pcl::registration::TransformationEstimationLM<pcl::PointXYZ, pcl::PointXYZ, double>::Matrix4 res_matrix;
-  pcl::UniformSampling<pcl::PointXYZ> uniform_sampling;
-  pcl::registration::CorrespondenceEstimation<pcl::PointXYZ, pcl::PointXYZ> initial_estimation;
-  pcl::registration::CorrespondenceRejectorDistance rejecter;
-  // int iteration;
-  // pcl::registration::DefaultConvergenceCriteria<double> criteria(iteration, trs_matrix, *rejected_correspondences);
-  // criteria.setMaximumIterations(100);
-  // criteria.setMaximumIterationsSimilarTransforms(10);
-  // criteria.setTranslationThreshold(0.1);
-  // criteria.setRotationThreshold(std::cos(0.5 * 3.141592 / 180.0));
-  // criteria.setRelativeMSE(0.001);
-  
-  seg.setOptimizeCoefficients (true);
 
+  pcl::PCLPointCloud2::<int> indices;
+  pcl::UniformSampling<pcl::PointXYZ> uniform_sampling;
+
+  pcl::CorrespondencesPtr corresps(new pcl::Correspondences);
+  pcl::registration::CorrespondenceEstimation<pcl::PointXYZ, pcl::PointXYZ> est;
+
+  reader.read("/home/haeyeon/Lidar/RIST_data/1.pcd", *source_cloud);
+  reader.read("/home/haeyeon/Lidar/RIST_data/2.pcd", *target_cloud);
+  est.setInputSource(source_cloud);
+  est.setInputTarget(target_cloud);
+  est.determineCorrespondences(*corresps, max_dist);
+
+  DefaultConvergenceCriteria<double> conv_crit (iteration, transform, *correspondences); 
+  conv_crit.setMaximumIterations (30);
+  conv_crit.setMaximumIterationsSimilarTransforms (3); 
+  conv_crit.setTranslationThreshold (5e-3);
+  conv_crit.setRotationThreshold (cos (0.5 * M_PI / 180.0)); 
+  conv_crit.setRelativeMSE (0.01); 
+  // do{(... ICP iterations ...)}  while (!conv_crit.hasConverged ())
+  // ConvergenceState why_conv =conv_crit.getConvergenceState ();
+
+  seg.setOptimizeCoefficients (true);
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setMaxIterations (1000);
   seg.setDistanceThreshold (0.2);
 
   for(int i = 1; i < 1200; i = i+100){  
-
+    
     std::string path = "/home/haeyeon/Lidar/RIST_data/";
     std::string ext = ".pcd";
     std::string filename = path + std::to_string(i) + ext;
@@ -67,7 +71,7 @@ int main ( int argc, char** argv )
     //uniform sampling
     uniform_sampling.setInputCloud(target);
     uniform_sampling.setRadiusSearch(0.03f);
-    uniform_sampling.filter(*target);
+    uniform_sampling.compute(indices);
 
     //remove ground plane
     seg.setInputCloud (source);
@@ -81,23 +85,6 @@ int main ( int argc, char** argv )
       target = source;
       continue;
     } 
-    // iteration = 0;
-    // res_matrix.setIdentity();
-    // initial_estimation.setInputSource(source);
-    // initial_estimation.setInputTarget(target);
-    // initial_estimation.determineCorrespondences(*correspondences, 0.1);
-    // rejecter.setInputSource<pcl::PointXYZ>(source);
-    // rejecter.setInputTarget<pcl::PointXYZ>(target);
-    // rejecter.setInputCorrespondences(correspondences);
-    // rejecter.setMaximumDistance(0.1);
-    // rejecter.getCorrespondences(*rejected_correspondences);
-    // transformation.estimateRigidTransformation(*source, *target, *rejected_correspondences, trs_matrix);
-    // pcl::transformPointCloud(*source, *source, trs_matrix);
-    // res_matrix = res_matrix * trs_matrix;
-    // pcl::transformPointCloud(result, result, res_matrix);
-    // result += *source;
-    // viewer.addPointCloud (source, "cloud"+std::to_string(i));
-    // viewer.spinOnce();
     //icp
     pcl::transformPointCloud(*source, *source, transformation_matrix);
     pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp; 
